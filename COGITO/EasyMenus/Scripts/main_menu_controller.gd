c328226998c00f9a0672b9e6dd61e95c9cc8e720
Phazorknight <70389309+Phazorknight@users.noreply.github.com>
2024-02-23 11:41:43 -0800

    Quickfix on ConfigErrors / not starting.
    - Replaced the old PauseMenu with the new TabMenu in all current DemoScenes.
    - Removed the Options from the MainMenu.
    This should now let you start the project without any errors getting thrown.
    The old PauseMenu and it's related options_menu script will no longer work with the current setup. Will look into officially deprecating it very soon.

--- COGITO/EasyMenus/Scripts/main_menu_controller.gd
+++ COGITO/EasyMenus/Scripts/main_menu_controller.gd
@@ -2,7 +2,6 @@ extends Control
 signal start_game_pressed
 
 @onready var start_game_button: Button = $%StartGameButton
-@onready var options_menu: Control = $%OptionsMenu
 @onready var content: Control = $%Content 
 
 #region UI AUDIO
@@ -51,14 +50,11 @@ func quit():
 	get_tree().quit()
 	
 func open_options():
-	options_menu.show()
 	content.hide()
-	options_menu.on_open()
 	
 func close_options():
 	content.show();
 	start_game_button.grab_focus()
-	options_menu.hide()
 
 
 func _on_start_game_button_pressed():
