74b2a9f159e0e0d98ca6a2826b5d0f55796865d4
kk1201 <kacperkuzma12@gmail.com>
2024-02-28 16:44:44 +0000

    Added switchable property to lightzone component, on by default.

--- COGITO/Components/LightzoneComponent.gd
+++ COGITO/Components/LightzoneComponent.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+@export var switchable: bool = true
+
 func _on_body_entered(body):
 	if body.is_in_group("Player") :
 		print("Player entered safezone.")
@@ -10,3 +12,7 @@ func _on_body_exited(body):
 	if body.is_in_group("Player") :
 		print("Player left dangerzone.")
 		body.brightness_component.subtract(1)
+
+func _on_switched(is_on):
+	if switchable:
+		set_monitoring(is_on)
