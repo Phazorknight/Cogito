f6ef44b9c1b641ca679ecd55756c29568a1961c5
kk1201 <kacperkuzma12@gmail.com>
2024-02-28 18:37:58 +0000

    Moving lightzone switch to its own component

--- COGITO/Components/LightzoneComponent.gd
+++ COGITO/Components/LightzoneComponent.gd
@@ -1,7 +1,5 @@
 extends Area3D
 
-@export var switchable: bool = true
-
 func _on_body_entered(body):
 	if body.is_in_group("Player") :
 		print("Player entered safezone.")
@@ -12,7 +10,3 @@ func _on_body_exited(body):
 	if body.is_in_group("Player") :
 		print("Player left dangerzone.")
 		body.brightness_component.subtract(1)
-
-func _on_switched(is_on):
-	if switchable:
-		set_monitoring(is_on)
