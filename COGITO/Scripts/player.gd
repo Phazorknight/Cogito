b697995037255a06f372f8ae89bda282af30103c
FailSpy <9100196+FailSpy@users.noreply.github.com>
2024-03-13 13:15:07 -0400

    Old layer config; raycast exclude player colliders

--- COGITO/Scripts/player.gd
+++ COGITO/Scripts/player.gd
@@ -138,6 +138,7 @@ func _ready():
 	#Some Setup steps
 	CogitoSceneManager._current_player_node = self
 	player_interaction_component.interaction_raycast = $Neck/Head/Eyes/Camera/InteractionRaycast
+	player_interaction_component.interaction_raycast.add_exception_rid(get_rid())
 	
 	randomize() 
 	
