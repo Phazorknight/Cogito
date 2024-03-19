e834bf51c0ecebf57fd8358aae4b94510daef9fc
Nutcrawler <53676617+Nutcrawler@users.noreply.github.com>
2024-03-18 20:22:49 -0500

    Basic light detection
    Added simple camera rendering a light-only debug draw, and set the visibility attribute based on the luminance of the texture

--- COGITO/Scripts/player.gd
+++ COGITO/Scripts/player.gd
@@ -112,6 +112,13 @@ var ladder_on_cooldown : bool = false
 @export var JOY_V_SENS : int = 3
 @export var JOY_H_SENS : int = 2
 
+#Light Detection Stuff
+@onready var sub_viewport := $SubViewport
+@onready var light_detection := $SubViewport/LightDetection
+@onready var texture_rect := $SubViewport/LightDetection/TextureRect
+@onready var color_rect := $SubViewport/LightDetection/ColorRect
+@onready var light_level := $LightLevel
+
 var joystick_h_event
 var joystick_v_event
  
@@ -363,6 +370,21 @@ func _process_on_ladder(_delta):
 		on_ladder = false
 
 var jumped_from_slide = false
+
+func get_average_color(texture: ViewportTexture) -> Color:
+	var image = texture.get_image() # Get the Image of the input texture
+	image.resize(1, 1, Image.INTERPOLATE_LANCZOS) # Resize the image to one pixel
+	return image.get_pixel(0, 0) # Read the color of that pixel
+
+func _process(_delta: float) -> void:
+	# Light detection
+	light_detection.global_position = global_position # Make light detection follow the player
+	var texture = sub_viewport.get_texture() # Get the ViewportTexture from the SubViewport
+	texture_rect.texture = texture # Display this texture on the TextureRect
+	var color = get_average_color(texture) # Get the average color of the ViewportTexture
+	color_rect.color = color # Display the average color on the ColorRect
+	#light_level.value = color.get_luminance() # Use the average color's brighness as the light level value
+	visibility_attribute.set_attribute(color.get_luminance() * 5, 5)
 
 func _physics_process(delta):
 	#if is_movement_paused:
