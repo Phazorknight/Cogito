f9fa0a55163145bc07fde0ea082d3d8e5f292602
aronand <71503746+aronand@users.noreply.github.com>
2024-04-18 22:46:54 +0300

    feat: Adds InteractionRayCast
    InteractionRayCast emits a signal whenever an interactable enters or
    leaves the player's view.

---
+++ COGITO/Scripts/interaction_raycast.gd
@@ -0,0 +1,42 @@
+class_name InteractionRayCast
+extends RayCast3D
+
+
+signal interactable_seen(interactable: CogitoObject)
+signal interactable_unseen()
+
+var _interactable: CogitoObject = null
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	_update_interactable()
+
+
+func _update_interactable() -> void:
+	var collider: Node = get_collider()
+	var new_interactable = collider
+
+	# Handle all colliders that aren't in interactable group as null.
+	if collider != null and not collider.is_in_group("interactable"):
+		new_interactable = null
+
+	# If interactable hasn't changed, no actions required. Because we treat
+	# non-interactables as null, we also avoid accidental false assignations.
+	if new_interactable == _interactable:
+		return
+
+	# If we got this far, we have unseen the currently tracked interactable
+	if _interactable != null:
+		interactable_unseen.emit()
+		print("UNSEEN")
+
+	_interactable = new_interactable
+
+	# If we have a new tracked interactable, we have seen an interactable
+	if _interactable != null:
+		interactable_seen.emit(new_interactable)
+		print("SEEN")
