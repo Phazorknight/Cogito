703cfbffb116dd80ea3520e3a6ff8beabc7eca1d
aronand <71503746+aronand@users.noreply.github.com>
2024-04-20 22:53:24 +0300

    refactor: Adds descriptive booleans to a long if statement

--- COGITO/Components/Interactions/PickupComponent.gd
+++ COGITO/Components/Interactions/PickupComponent.gd
@@ -15,15 +15,19 @@ func pick_up(_player_interaction_component: PlayerInteractionComponent):
 		return
 
 	# Update wieldable UI if we have picked up ammo for current wieldable
-	# TODO: Replace with a better solution
+	# TODO: Possibly replace with a better solution, maybe by signaling the change
+	# to the UI instead of having the PlayerInteractionComponent doing it.
 	if _player_interaction_component.is_wielding:
-		if "reload_amount" in slot_data.inventory_item and _player_interaction_component.equipped_wieldable_item.ammo_item_name == slot_data.inventory_item.name:
+		var is_ammo: bool = "reload_amount" in slot_data.inventory_item
+		var is_current_ammo: bool = _player_interaction_component.equipped_wieldable_item.ammo_item_name == slot_data.inventory_item.name
+		if is_ammo and is_current_ammo:
 			_player_interaction_component.equipped_wieldable_item.update_wieldable_data(_player_interaction_component)
 
 	_player_interaction_component.send_hint(slot_data.inventory_item.icon, slot_data.inventory_item.name + " added to inventory.")
 	was_interacted_with.emit(interaction_text, input_map_action)
 	Audio.play_sound(slot_data.inventory_item.sound_pickup)
 	self.get_parent().queue_free()
+
 
 func get_item_type() -> int:
 	if slot_data and slot_data.inventory_item:
