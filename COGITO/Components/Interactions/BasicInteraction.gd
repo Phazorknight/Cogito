21078e3346fdd62033b6e36494d2ecbeea8bbbd6
Phazorknight <70389309+Phazorknight@users.noreply.github.com>
2024-07-16 12:26:11 -0700

    Fixing some BasicInteraction checks for more generic uses.
    BasicInteraction.gd:
    - Added signal check.
    - Added method check for interact.
    
    This now enables the BasicInteraction to be used without any methods on the parent. Useful if one only wants to use the signals.

--- COGITO/Components/Interactions/BasicInteraction.gd
+++ COGITO/Components/Interactions/BasicInteraction.gd
@@ -1,14 +1,17 @@
 extends InteractionComponent
 
-@onready var parent_node = get_parent() #Grabbing reference to door
+@onready var parent_node = get_parent() #Grabbing reference to parent
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	parent_node.object_state_updated.connect(_on_object_state_change)
-
+	if parent_node.has_signal("object_state_updated"):
+		parent_node.object_state_updated.connect(_on_object_state_change)
 
 func interact(_player_interaction_component):
-	parent_node.interact(_player_interaction_component)
+	if parent_node.has_method("interact"):
+		parent_node.interact(_player_interaction_component)
+	
+	was_interacted_with.emit(interaction_text,input_map_action)
 
 
 func _on_object_state_change(_interaction_text: String):
