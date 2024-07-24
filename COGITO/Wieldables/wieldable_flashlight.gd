d3edb7b51a93ac268f2598882ac478a4043aa29c
Kevin Snejstrup <krysis@duck.com>
2024-07-24 18:06:42 +0200

    Issue #213 Flashlight fix + more features
    - Fixed issue where flashlight does not turn on / off while running, walking, and spam clicking.
    - Added cooldown for flashlight, so it cannot be rapidly turned on / off (for better immersion, but can ofc be adjusted)
    - Flashlight animation will finish before it will play again (better immersion!)

--- COGITO/Wieldables/wieldable_flashlight.gd
+++ COGITO/Wieldables/wieldable_flashlight.gd
@@ -19,6 +19,12 @@ extends CogitoWieldable
 var trigger_has_been_pressed : bool = false
 
 
+var is_action_pressed : bool = false # Prevent flashlight failing to turn on / off while walking / running.
+var can_toggle : bool = true # If the flashlight can be toggled.
+var toggle_cooldown : float = 0.5  # Adjust this value to set the cooldown time for flashlight to turn on / off.
+var cooldown_timer : float = 0.0
+
+
 func _ready():
 	if wieldable_mesh:
 		wieldable_mesh.hide()
@@ -34,15 +40,24 @@ func _process(delta):
 		if player_interaction_component.equipped_wieldable_item.charge_current == 0:
 			turn_off()
 			is_on = false
+    
+	if not can_toggle:
+		cooldown_timer += delta
+		if cooldown_timer >= toggle_cooldown:
+			can_toggle = true
+			cooldown_timer = 0.0
 
-
-# Primary action called by the Player Interaction Component when flashlight is wielded.
-func action_primary(_passed_item_reference:InventoryItemPD, _is_released: bool):
-	if _is_released:
+func action_primary(_passed_item_reference:InventoryItemPD, is_released: bool):
+	if is_released:
+		is_action_pressed = false
 		return
-		
-	animation_player.play(anim_action_primary)
-	toggle_on_off()
+    
+	if not is_action_pressed and can_toggle and not animation_player.is_playing():
+		is_action_pressed = true
+		can_toggle = false
+		cooldown_timer = 0.0
+		animation_player.play(anim_action_primary)
+		toggle_on_off()
 
 
 # Function called when wieldable is unequipped.
@@ -76,7 +91,7 @@ func turn_off():
 func toggle_on_off():
 	audio_stream_player_3d.stream = switch_sound
 	audio_stream_player_3d.play()
-	
+    
 	if is_on:
 		spot_light_3d.hide()
 		is_on = false
