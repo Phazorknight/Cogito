23cc9e499aa8877be94a246b0e45d48e90a531b0
nvvv <67618133+ZeroHanekoma@users.noreply.github.com>
2024-08-15 19:31:22 -0700

    Add files via upload
    some alterations to allow for the splash effect (WHICH I FIXED) and sounds.

--- addons/floatable_body/fluid_area_3d.gd
+++ addons/floatable_body/fluid_area_3d.gd
@@ -39,10 +39,32 @@ func body_exit(body: PhysicsBody3D) -> void:
 func get_height(pos: Vector3) -> float:
 	return surface_height + 0.3 * noise.get_noise_3d(time * 2.0 + pos.x, pos.y, time * 5.0 + pos.z)	
 
+const WATER_SPLASH = preload("res://game/objects/water_splash.tscn")
 
-func _on_body_entered(body):
+var splash_cooldown: float = 2000  # Cooldown in milliseconds
+var last_splash_time: Dictionary = {}
+
+func _on_body_entered(body: PhysicsBody3D):
 	if body is PhysicsBody3D:
+		var current_time = Time.get_ticks_msec()
+		var body_id = body.get_instance_id()
+
+		# Check if the body is allowed to make a splash
+		if body_id in last_splash_time:
+			var time_since_last_splash = current_time - last_splash_time[body_id]
+			if time_since_last_splash < splash_cooldown:
+				return  # Skip the splash if cooldown hasn't passed
+
+		# Create splash and set the position to the water surface
+		var splash_instance = WATER_SPLASH.instantiate()
+		add_child(splash_instance)
+		splash_instance.global_position = Vector3(body.global_position.x, body.global_position.y, body.global_position.z)
+		
+		print(splash_instance.global_position.y, "/", body.global_position.y,".  Surface Height: ", surface_height)
+		# Update last splash time for this body
+		last_splash_time[body_id] = current_time
 		body_enter(body)
+
 
 
 func _on_body_exited(body):
