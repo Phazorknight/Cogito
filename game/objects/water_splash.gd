16ba4a0a2722d27e11ee1c72aa64b05a767afab3
nvvv <67618133+ZeroHanekoma@users.noreply.github.com>
2024-08-15 19:32:16 -0700

    Add files via upload
    fixed the splash effect, i'm an idiot.

--- game/objects/water_splash.gd
+++ game/objects/water_splash.gd
@@ -1,13 +1,23 @@
 extends Node3D
 @onready var splash_stream_player_3d = $SplashStreamPlayer3D
 @onready var droplets = $Droplets
-@onready var steam = $Steam
 
 
 func _ready():
 	droplets.emitting = true
-	steam.emitting = true
 	splash_stream_player_3d.play()
 
-func _on_steam_finished():
+func _on_droplets_finished():
+	if splash_stream_player_3d.playing != true:
+		die()
+
+func _on_splash_stream_player_3d_finished():
+	if droplets.emitting != true:
+		die()
+
+func die():
 	queue_free()
+
+func _on_timer_timeout():
+	#if it's alive too long kill it.
+	die()
