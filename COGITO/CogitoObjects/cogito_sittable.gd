d362511a18bac4a5bddce3af923b4bd024676a89
niefia <99533148+niefia@users.noreply.github.com>
2024-09-05 10:15:50 +0100

    Added "disable_carry" property
    Disables Carryable component if found, currently checks for this at the sibling level.
    This option is to avoid some issues with Sittable components on physics bodies, so is set to on by default

--- COGITO/CogitoObjects/cogito_sittable.gd
+++ COGITO/CogitoObjects/cogito_sittable.gd
@@ -4,10 +4,15 @@ class_name CogitoSittable
 
 signal object_state_updated(interaction_text: String) #used to display correct interaction prompts
 
+##Path to node used as the Sit marker
 @export var sit_position_node_path: NodePath  
 @export var tween_duration: float = 1.0
+##Interaction text when Sat Down
 @export var interaction_text_when_on : String = "Stand Up"
+##Interction text when not Sat Down
 @export var interaction_text_when_off : String = "Sit"
+##Disables (sibling) Carryable Component if found
+@export var disable_carry : bool = true
 
 var interaction_text : String 
 var player_node: Node3D = null
@@ -15,6 +20,7 @@ var sit_position_node: Node3D = null
 var is_player_sitting: bool = false
 var original_position: Transform3D  
 var interaction_nodes : Array[Node]
+var carryable_components: Array = [Node]
 
 func _ready():
 	#find player node
@@ -28,7 +34,22 @@ func _ready():
 	sit_position_node = get_node(sit_position_node_path)
 	if not sit_position_node:
 		print("Sit position node not found. Ensure the path is correct.")
+		
+	if disable_carry:
+		carryable_components = get_sibling_carryable_components()
 
+func get_sibling_carryable_components() -> Array:
+	var components = []
+	var parent = get_parent()
+	
+	# iterate through sibling nodes and check for CogitoCarryableComponent
+	for child in parent.get_children():
+		if child is CogitoCarryableComponent:
+			components.append(child)
+	
+	return components
+	
+	
 func _sit_down():
 	player_node = CogitoSceneManager._current_player_node
 	
@@ -45,6 +66,10 @@ func _sit_down():
 		tween.tween_property(player_node, "global_transform", sit_position_node.global_transform, tween_duration)
 		tween.tween_callback(Callable(self, "_on_tween_finished"))
 		is_player_sitting = true
+		
+		if disable_carry:
+			for component in carryable_components:
+				component.is_disabled = true
 
 func _on_tween_finished():
 	player_node.global_transform = sit_position_node.global_transform
@@ -60,6 +85,10 @@ func _stand_up():
 		var tween = create_tween()
 		tween.tween_property(player_node, "global_transform", original_position, tween_duration)
 		tween.tween_callback(Callable(self, "_on_stand_up_finished"))
+		
+		if disable_carry:
+			for component in carryable_components:
+				component.is_disabled = false
 
 func _on_stand_up_finished():
 	player_node.is_sitting = false  
