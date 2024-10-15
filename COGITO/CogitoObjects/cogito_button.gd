c682930c3ffa1ef28794bb94aac57c20bf0b640a
niefia <99533148+niefia@users.noreply.github.com>
2024-10-15 12:12:06 +0100

    Specific currency check & symbol
    Now checks for specified currency, and includes a symbol for currency
    Need to make the icon accessed automatically, but may be best to setup a global store of currencies to avoid doing this through the player.
    
    Interaction UI Component label will need reworking as TextLabel replaced with RichTextLabel to support the currency image being included

--- COGITO/CogitoObjects/cogito_button.gd
+++ COGITO/CogitoObjects/cogito_button.gd
@@ -3,7 +3,8 @@ extends Node3D
 signal object_state_updated(interaction_text: String) #used to display correct interaction prompts
 signal pressed()
 signal damage_received(damage_value:float)
-
+signal transaction_success
+signal transaction_failed
 
 @export_group("Cogito Button Settings")
 ## Sound that plays when pressed.
@@ -16,8 +17,6 @@ signal damage_received(damage_value:float)
 @export var press_cooldown_time : float
 ## Hint that displays after this has been used.
 @export var has_been_used_hint : String
-##Hint that displays if player doesn't have enough currency to interact
-@export var not_enough_currency_hint : String
 ## Prompt text when button has been pressed and can't be used repeatedly.
 @export var unusable_interaction_text : String = "Unavailable"
 ## Check this if player needs to have an item in the inventory to switch.
@@ -27,24 +26,32 @@ signal damage_received(damage_value:float)
 ## Hint that gets displayed if the switch requires an item that the player currently doesn't have.
 @export var item_hint : String
 
-##How much it costs to use this button
-@export var currency_cost : int  = 0
-
-var currency_text : String
-##Text that joins Press & Cost for Interaction 
-@export var currency_text_joiner : String = " | Cost: "
-
 ## Nodes that will have their interact function called when this switch is used.
 @export var objects_call_interact : Array[NodePath]
 @export var objects_call_delay : float = 0
 
 
+@export_subgroup("Currency Interaction")
+##How much it costs to use this button, If set to 0 currency interaction will be ignored
+@export var currency_cost : int  = 0
+##Text that joins Press & Cost for Interaction 
+@export var currency_text_joiner : String = " | Cost: "
+##Hint that displays if player doesn't have enough currency to interact
+@export var not_enough_currency_hint : String
+##Name of the currency needed for this interaction to proceed - default is credits
+@export var currency_name : String = "credits"
+@export var currency_icon = "res://COGITO/Assets/Graphics/UiIcons/Ui_Icon_Currency.png"
+
+
+var icon_bbcode : String
 var has_been_used : bool = false
 var interaction_text : String 
 var player_interaction_component : PlayerInteractionComponent
 var interaction_nodes : Array[Node]
 var cogito_properties : CogitoProperties = null
 var cooldown : float
+var currency_text : String
+
 
 func _ready() -> void:
 	self.add_to_group("interactable")
@@ -52,14 +59,18 @@ func _ready() -> void:
 	interaction_nodes = find_children("","InteractionComponent",true) #Grabs all attached interaction components
 	cooldown = 0 #Enabling button to be pressed right away.
 	if currency_cost != 0: 
-		currency_text = currency_text_joiner + str(currency_cost)
+		icon_bbcode = "[img width=16 height=16]" + currency_icon + "[/img]"
+		currency_text = currency_text_joiner + str(currency_cost)  + icon_bbcode
 	interaction_text = usable_interaction_text  + currency_text
 	object_state_updated.emit(interaction_text)
 	find_cogito_properties()
+
+
 func find_cogito_properties():
 	var property_nodes = find_children("","CogitoProperties",true) #Grabs all attached property components
 	if property_nodes:
 		cogito_properties = property_nodes[0]
+
 
 func _physics_process(delta: float) -> void:
 	if cooldown > 0:
@@ -71,9 +82,10 @@ func interact(_player_interaction_component:PlayerInteractionComponent):
 		return
 		
 	player_interaction_component = _player_interaction_component
-	if !check_for_currency():
-		player_interaction_component.send_hint(null, not_enough_currency_hint)
-		return
+	if currency_cost != 0:
+		if !check_for_currency():
+			player_interaction_component.send_hint(null, not_enough_currency_hint)
+			return
 	if !allows_repeated_interaction and has_been_used:
 		player_interaction_component.send_hint(null, has_been_used_hint)
 		return
@@ -103,7 +115,7 @@ func press():
 			object.interact(player_interaction_component)
 
 
-func _on_damage_received():
+func _on_damage_received(damage):
 	interact(CogitoSceneManager._current_player_node.player_interaction_component)
 
 
@@ -121,22 +133,26 @@ func check_for_item() -> bool:
 		player_interaction_component.send_hint(null,item_hint) # Sends the key hint with the default hint icon.
 	return false
 
+
 func check_for_currency() -> bool:
 	var found_currency
 	var player = player_interaction_component.get_parent()
 	for attribute in player.find_children("", "CogitoCurrency", false):
 		if attribute is CogitoCurrency:
-			found_currency = attribute
+			if attribute.currency_name == currency_name:
+				found_currency = attribute
 			break
-	
+
 	if found_currency.value_current >= currency_cost:
 		##Player has enough currency, apply transaction and return true
 		found_currency.value_current = found_currency.value_current-currency_cost
+		transaction_success.emit()
 		return true 
 	else:
 		##Player doesn't have enough currency, don't apply transaction and return false
+		transaction_failed.emit()
 		return false
-	
+
 
 func set_state():
 	if has_been_used:
--- COGITO/Components/UI/UI_PromptComponent.gd
+++ COGITO/Components/UI/UI_PromptComponent.gd
@@ -2,7 +2,7 @@ extends Control
 class_name UiPromptComponent
 
 @onready var interaction_button: Node = $HBoxContainer/Container/InteractionButton
-@onready var interaction_text: Label = $HBoxContainer/InteractionText
+@onready var interaction_text: RichTextLabel = $HBoxContainer/InteractionText
 
 func set_prompt(interaction_name:String,input_map_name:String):
 	interaction_button.action_name = input_map_name
