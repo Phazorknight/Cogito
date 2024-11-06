faf0f1383e194d12a708d82475695281fcbfa400
Phazorknight <70389309+Phazorknight@users.noreply.github.com>
2024-11-05 17:25:45 -0800

    Wieldable consumable + plug in input map reset
    NEW: Wieldable consumable
    - This type of wieldable is meant for consumable items.
    - They only play their equip animation and then get unequipped.
    - Their effects get triggered in the animation track (so make sure you call the method in your animation!)
    - See the Health Extension item in the Laboratory scene for a basic example.
    - Issues: Wieldable switching seems to bug out a bit after using a wieldable consumable. Need to investigate more.
    
    Cogito Plugin:
    - There's now a button that resets/overrides the project's input map with the Cogito defaults.
    - This is currently work in progress, not all input actions are being applied.
    - Meant to be used when people install cogito by just importing the plug-in folder (which doesn't apply any input actions). This way, people can press this once after installing Cogito and their input map should be configured correctly.

---
+++ addons/cogito/Wieldables/wieldable_consumable.gd
@@ -0,0 +1,47 @@
+extends CogitoWieldable
+
+@export var consumable_effects : Array[ConsumableEffect]
+
+var player_inventory : CogitoInventory
+var item_slot : InventorySlotPD
+
+# Function called when wieldable is unequipped.
+func equip(_player_interaction_component: PlayerInteractionComponent):
+	player_interaction_component = _player_interaction_component
+	player_inventory = player_interaction_component.get_parent().inventory_data
+	item_slot = get_slot_reference()
+	player_interaction_component.updated_wieldable_data.emit(null,0,null)
+	
+	animation_player.play(anim_equip)
+	await get_tree().create_timer(animation_player.current_animation_length).timeout
+	
+	item_reference.is_being_wielded = false
+	player_interaction_component.change_wieldable_to(null)
+	
+	unequip()
+	if wieldable_mesh:
+		wieldable_mesh.hide()
+
+
+func apply_effects():
+	for effect in consumable_effects:
+		effect.use(player_interaction_component.get_parent())
+	
+	### Reduce the stack of the dart items:
+	if item_slot:
+		item_slot.quantity -= 1
+		if item_slot.quantity < 1:
+			player_inventory.remove_slot_data(item_slot)
+			
+	player_inventory.inventory_updated.emit(player_inventory)
+
+
+# Function to get item slot
+func get_slot_reference() -> InventorySlotPD:
+	var slot_reference : InventorySlotPD
+	if player_inventory != null:
+		for slot in player_inventory.inventory_slots:
+			if slot != null and slot.inventory_item.name == item_reference.name:
+				slot_reference = slot
+				
+	return slot_reference
--- addons/cogito/cogito_main.gd
+++ addons/cogito/cogito_main.gd
@@ -57,3 +57,91 @@ func _on_btn_git_hub_pressed() -> void:
 
 func _on_btn_documentation_pressed() -> void:
 	OS.shell_open("https://cogito.readthedocs.io/en/latest/index.html")
+
+
+func _on_btn_reset_input_map_pressed() -> void:
+	
+	var keyboard_key = InputEventKey.new()
+	var mouse_button = InputEventMouseButton.new()
+	var gamepad_button = InputEventJoypadButton.new()
+	var gamepad_motion = InputEventJoypadMotion.new()
+	
+	keyboard_key = InputEventKey.new()
+	keyboard_key.physical_keycode = KEY_SPACE
+	gamepad_button = InputEventJoypadButton.new()
+	gamepad_button.device = -1
+	gamepad_button.button_index = JOY_BUTTON_A
+	save_input_action_to_settings("jump",keyboard_key,gamepad_button)
+	
+	keyboard_key = InputEventKey.new()
+	keyboard_key.physical_keycode = KEY_C
+	gamepad_button = InputEventJoypadButton.new()
+	gamepad_button.device = -1
+	gamepad_button.button_index = JOY_BUTTON_B
+	save_input_action_to_settings("crouch",keyboard_key,gamepad_button)
+	
+	keyboard_key = InputEventKey.new()
+	keyboard_key.physical_keycode = KEY_SHIFT
+	gamepad_button = InputEventJoypadButton.new()
+	gamepad_button.device = -1
+	gamepad_button.button_index = JOY_BUTTON_LEFT_STICK
+	save_input_action_to_settings("sprint",keyboard_key,gamepad_button)
+	
+	keyboard_key = InputEventKey.new()
+	keyboard_key.physical_keycode = KEY_ESCAPE
+	gamepad_button = InputEventJoypadButton.new()
+	gamepad_button.device = -1
+	gamepad_button.button_index = JOY_BUTTON_START
+	save_input_action_to_settings("menu",keyboard_key,gamepad_button)
+	
+	keyboard_key = InputEventKey.new()
+	keyboard_key.physical_keycode = KEY_F
+	gamepad_button = InputEventJoypadButton.new()
+	gamepad_button.device = -1
+	gamepad_button.button_index = JOY_BUTTON_X
+	save_input_action_to_settings("interact",keyboard_key,gamepad_button)
+	
+	keyboard_key = InputEventKey.new()
+	keyboard_key.physical_keycode = KEY_E
+	gamepad_button = InputEventJoypadButton.new()
+	gamepad_button.device = -1
+	gamepad_button.button_index = JOY_BUTTON_Y
+	save_input_action_to_settings("interact2",keyboard_key,gamepad_button)
+	
+	keyboard_key = InputEventKey.new()
+	keyboard_key.physical_keycode = KEY_TAB
+	gamepad_button = InputEventJoypadButton.new()
+	gamepad_button.device = -1
+	gamepad_button.button_index = JOY_BUTTON_BACK
+	save_input_action_to_settings("inventory",keyboard_key,gamepad_button)
+	
+	mouse_button.button_index = MOUSE_BUTTON_LEFT
+	gamepad_motion = InputEventJoypadMotion.new()
+	gamepad_motion.device = -1
+	gamepad_motion.axis = JOY_AXIS_TRIGGER_RIGHT
+	save_input_action_to_settings("action_primary",mouse_button,gamepad_motion)
+	
+	mouse_button.button_index = MOUSE_BUTTON_RIGHT
+	gamepad_motion = InputEventJoypadMotion.new()
+	gamepad_motion.device = -1
+	gamepad_motion.axis = JOY_AXIS_TRIGGER_LEFT
+	save_input_action_to_settings("action_secondary",mouse_button,gamepad_motion)
+	
+	# I've not found a way to update the project settings input map editor but to restart the whole editor.
+	EditorInterface.restart_editor(true)
+
+
+func save_input_action_to_settings(input_name: String, input_kbm:InputEventWithModifiers, input_gamepad:InputEvent):
+	var input = {
+		"deadzone": 0.5,
+		"events": [
+			input_kbm,
+			input_gamepad
+		]
+	}
+	
+	var setting_name = 'input/' + input_name
+	# Set the input/<name_of_your_input_action> in the project settings, then save the settings.
+	ProjectSettings.set_setting(setting_name, input)
+	ProjectSettings.save()
+	print("COGITO: Saved input to settings: ", setting_name, ". input_kbm=", input_kbm, ". input_gamepad=", input_gamepad)
