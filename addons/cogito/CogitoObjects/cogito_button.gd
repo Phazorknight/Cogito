c8e0d847f5f92937a982916e20e78f30c4aba308
Brody Brooks <brody@brodybrooks.com>
2025-02-06 13:15:50 -0800

    cogito_button: press signal now passes button instance

--- addons/cogito/CogitoObjects/cogito_button.gd
+++ addons/cogito/CogitoObjects/cogito_button.gd
@@ -2,7 +2,7 @@
 extends Node3D
 class_name CogitoButton
 signal object_state_updated(interaction_text: String) #used to display correct interaction prompts
-signal pressed()
+signal pressed(button: CogitoButton)
 signal damage_received(damage_value:float)
 
 
@@ -94,7 +94,7 @@ func press():
 			player_interaction_component.send_hint(null, currency_check.not_enough_currency_hint)
 			return
 	
-	pressed.emit()
+	pressed.emit(self)
 	Audio.play_sound_3d(press_sound).global_position = global_position
 	
 	if !allows_repeated_interaction:
